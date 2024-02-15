a29372a18cd71b8a93ce05de8411cce50c444505
Ingyinn Swe <enjeanswe@gmail.com>
2024-02-16 00:52:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-
-import React from 'react';
-import Info from './pages/Info';
+import React from "react";
+import Info from "./pages/Info";
 
 function App() {
   return (
-    <>
-    <Info name="Ingyinn Swe" nickname="Jean" jobTitle="Teacher Assistant"></Info>
-    </>
-
+    <main className="md:container md:mx-auto">
+      <div className="gap-8 columns-4 mx-auto my-20">
+        <Info
+          className="w-full"
+          imageURL= "/src/assets/Profile/image-1.jpg"
+          name="Dr. Kritsada Sriphaew"
+          nickname="Aj.Kong"
+          jobTitle="Asst.Prof & Dean of the RIC"
+        ></Info>
+        <Info
+          className="w-full"
+          imageURL= "/src/assets/Profile/image-2.jpg"
+          name="Prinda Setabundhu"
+          nickname="Aj.Pap"
+          jobTitle="Associate Dean for Student Affairs"
+        ></Info>
+        <Info
+          className="w-full"
+          imageURL= "/src/assets/Profile/image-3.jpg"
+          name="Amporn Puapradit"
+          nickname="Aj.Amporn"
+          jobTitle="Associate Dean for Academic Affairs"
+        ></Info>
+        <Info
+          className="w-full"
+          imageURL= "/src/assets/Profile/image-4.jpg"
+          name="Malisuwan Ussanee"
+          nickname="Aj.Koy"
+          jobTitle="Associate Dean for Administration"
+          ></Info>
+        
+        
+      </div>
+    </main>
   );
 }
 
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,30 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-function Info (props){
-    return (
-        <div className="card mx-auto ">
-            <img src="/src/assets/ric-logo.png" alt="Profile" className='h-56 w-auto'/>
-            <h1>{props.name}</h1>
-            <p>{props.nickname}</p>
-            <strong>{props.jobTitle}</strong>
-            <div>
-                <a href="path_to_ratings_page">Rating 1</a>
-                <a href="path_to_ratings_page">Rating 2</a>
-                <a href="path_to_ratings_page">Rating 3</a>
-            </div>
+function Info(props) {
+  return (
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img
+        src={props.imageURL}
+        alt="Profile"
+        className="mx-auto rounded-2xl w-full p-4"
+      />
+
+      <div className="px-5 pb-5">
+        <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-black">
+          {props.name}
+        </h3>
+        <p className="text-sm leading-6 text-gray-400">{props.jobTitle}</p>
+        <p className="text-base leading-7 text-gray-800">{props.nickname}</p>
+
+        <div className="my-4">
+          <a
+            type="button"
+            class="text-gray-900 bg-gray-300 hover:bg-gray-500 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2"
+          >
+            Euphoric
+          </a>
+          <a
+            type="button"
+            class="text-gray-900 bg-gray-300 hover:bg-gray-500  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2"
+          >
+            Innovative
+          </a>
+          <button
+            type="button"
+            class="text-gray-900 bg-gray-300 hover:bg-gray-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2"
+          >
+            Supervision
+          </button>
+          <button
+            type="button"
+            class="text-gray-900 bg-gray-300 hover:bg-gray-500 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2"
+          >
+            Counter Balance
+          </button>
         </div>
-    );
-};
-Info.propTypes = {
-    name: PropTypes.string.isRequired,
-    nickname: PropTypes.string.isRequired,
-    jobTitle: PropTypes.string.isRequired
+      </div>
+    </div>
+  );
 }
+Info.propTypes = {
+  name: PropTypes.string.isRequired,
+  nickname: PropTypes.string.isRequired,
+  jobTitle: PropTypes.string.isRequired,
+};
 
 Info.defaultProps = {
-    name: "Ajarn",
-    nickname: "unknown",
-    jobTitle: "Officer"
-}
+  name: "Ajarn",
+  nickname: "unknown",
+  jobTitle: "Officer",
+};
 export default Info;
