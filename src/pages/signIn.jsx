6e67f9338d8d658ea0b3bea634c8a64b2791a9a3
Khant Htoo Naing <khanthtoonaing2000@gmail.com>
2024-02-16 14:28:26 +0700

    className update

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -36,7 +36,7 @@ function signIn() {
             className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
 
-          <button type="submit" className="flex w-full mt-5 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <button type="submit" className="flex justify-center w-full mt-5 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Sign In
           </button>
         </form>
